4c1efbea73a88bb0f3fa401954b9d75847e3cfc3
Vivek Verma <bookyourown.mutual@gmail.com>
2022-09-22 00:52:09 +0530

    init

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
-        <title>Book your Own - Admin Portal</title>
+        <title>Book your Own - Admin</title>
         <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="./assets/css/index.min.css" />
